146201bbc603ffaac4ce451ffa56f36d8a0414f5
David Mah <mahhaha@gmail.com>
2014-01-09 15:45:17 -0800

    Change deploy script to use new executing binary

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,12 @@
-#!/bin/sh
+#!/bin/bash
 # This script sets all of the puzzle files to be owned by root, and all of the
 # run scripts to be setuid. This is because some puzzles need root in order to
 # run
 # This script must be run as root
 
 if [[ "$(id -u)" == "0" ]]; then
-    chown -R root:root .
-    chmod -R 644 .
-    find . -name "run.sh" -exec chmod +xs {} \;
+    find . -name "run.bin" -exec chown root:root {} \;
+    find . -name "run.bin" -exec chmod +xs {} \;
 else
     echo "This script must be run as root!"
     exit 1
